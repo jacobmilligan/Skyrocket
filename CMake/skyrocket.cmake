4abfb64535b11ea39e3b482489c039df439c70e2
Jacob Milligan <jacobpmilligan@gmail.com>
2018-01-21 23:00:26 +1100

    docs/cmake: Add header comments to cmake files

--- CMake/skyrocket.cmake
+++ CMake/skyrocket.cmake
@@ -1,6 +1,7 @@
 include(${PROJECT_SOURCE_DIR}/CMake/Skyrocket/platform.cmake)
 include(${PROJECT_SOURCE_DIR}/CMake/Skyrocket/graphics.cmake)
 
+# Initializes all cmake functions required by skyrocket
 function(skyrocket_init)
     unset(skyrocket_sources CACHE)
     unset(skyrocket_libraries CACHE)
@@ -26,7 +27,7 @@ function(skyrocket_init)
     skyrocket_add_definitions(-DSKY_${skyrocket_build_type})
 endfunction()
 
-# Adds source files
+# Adds source files to the projects global list
 function(skyrocket_add_sources)
     set(srcs)
     foreach(s IN LISTS ARGN)
@@ -39,6 +40,7 @@ function(skyrocket_add_sources)
     set(skyrocket_sources ${skyrocket_sources} "${srcs}" CACHE INTERNAL "")
 endfunction()
 
+# Adds all sources gathered in child directories to a library target
 function(skyrocket_add_library lib_name lib_type)
     if (${CMAKE_GENERATOR} STREQUAL "Xcode")
         file(GLOB_RECURSE headers ${CMAKE_CURRENT_SOURCE_DIR}/*.h
@@ -56,11 +58,14 @@ function(skyrocket_add_library lib_name lib_type)
     unset(skyrocket_sources CACHE)
 endfunction()
 
+# Creates a new list of sources to attach to a specific skyrocket module - doesn't necessarily have
+# to be associated with a library - could be a component etc.
 function(skyrocket_new_module module_name)
     set(${module_name}_sources "${skyrocket_sources}" CACHE INTERNAL "")
     unset(skyrocket_sources CACHE)
 endfunction()
 
+# Adds definitions to the global definitions list for the project
 function(skyrocket_add_definitions)
     foreach (def IN LISTS ARGN)
         add_definitions(${def})
@@ -68,6 +73,7 @@ function(skyrocket_add_definitions)
     endforeach ()
 endfunction()
 
+# Includes all previously added definitions to the entire project
 function(skyrocket_include_all_definitions)
     foreach(def IN LISTS skyrocket_definitions)
         add_definitions("${def}")
